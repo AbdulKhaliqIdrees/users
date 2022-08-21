65b314f3f29263ac876f4f4c2615fc620dbd35b9
AbdulKhaliqIdrees <abdulkhaliqabdulkhaliq076@gmail.com>
2022-08-21 06:47:25 -0700

    user1

--- lib/views/common_file.dart
+++ lib/views/common_file.dart
@@ -1,4 +1,29 @@
-/*Row(
+
+         /*Column(
+        children: [
+          SizedBox(
+            height: 70,
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: ApBarForMobile(),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          BodyForMobile(),
+          SizedBox(
+            height: 15,
+          ),
+          Expanded(
+            child: BotomNavigationBar(),
+          ),
+        ],
+      );*/
+      /*Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Row(
           children: [
             SideBar(),
             SizedBox(
@@ -6,4 +31,5 @@
             ),
             BodyForChrome(),
           ],
+        ),
         ),*/
--- lib/views/my_home.dart
+++ lib/views/my_home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:users/views/ap_bar_for_mobile.dart';
-import 'package:users/views/body_for_mobile.dart';
-import 'package:users/views/botom_navigation_bar.dart';
+import 'package:flutter/rendering.dart';
+import 'package:users/views/sidebar_chrome.dart';
+import 'package:users/views/bodyfor_chrome.dart';
 
 class MyHome extends StatelessWidget {
   const MyHome({Key? key}) : super(key: key);
@@ -11,6 +11,14 @@ class MyHome extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xffFFF0E9),
       body: SafeArea(
+        child:LayoutBuilder(
+          builder: (context, constraints){
+            return ListView(
+              children: [
+                if(constraints.maxWidth<=400){
+                   Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
           child: Column(
         children: [
           SizedBox(
@@ -31,7 +39,28 @@ class MyHome extends StatelessWidget {
             child: BotomNavigationBar(),
           ),
         ],
-      )),
+      ),
+        ),
+                }else{
+                  Container(
+                    height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child:Row(
+          children: [
+            SideBar(),
+            SizedBox(
+              width: 200,
+            ),
+            BodyForChrome(),
+          ],
+        ),
+                  ),
+                }
+              ],
+            );
+          } 
+          ),
+      ),
     );
   }
 }
